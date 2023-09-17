cec34311dff38024700e0d3ff2d3c882fa936ee7
TALLADA SAI SHARAN <137127716+talladasaisharan393@users.noreply.github.com>
2023-09-17 19:17:10 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,76 @@
+print("HEllo world!")
+
+#comments
+
+"""
+multiline comments
+"""
+
+car="Ferari are love!!!!!!!"
+
+print(car)
+
+print(type(car))
+
+car=2
+car=2.0
+
+print(type(car))
+
+a=20
+print(float(a))
+
+b=2.0
+print(int(b))
+
+
+c=7
+d=str(c)
+print(type(d))
+
+e="Hello sai ,how are uhh??"
+
+print(e)
+print(e[0])
+print(len(e))
+
+for i in e:
+    print(i)
+
+for i in range(11):
+    print(i)
+
+for i in range(1,11):
+    print(i)
+
+
+print(e.upper())
+print(e.lower())
+print(e.endswith("i"))
+
+print(10<=5)
+
+g=12
+n=16
+
+if g>n:
+    print("False it is!!")
+else:
+    print("Wowww it's truee!!")
+
+#FUNCTION-ARGUMENT,PARAMETER
+
+def greeting():
+    print("hello welcome to ismarrttt tutors!!!!!!")
+
+def greetings(name):
+    print("hello" + name)
+greetings("ISmart")
+
+#name is parameter
+#ismart is argument
+
+def add(u,i):
+    print(u+i)
+add(1,2)
+
